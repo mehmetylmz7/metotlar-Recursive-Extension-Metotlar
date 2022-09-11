65aee6022000ec66059015c71c3ce6d6c4a07f68
Mehmet Yılmaz <109681167+mehmetylmz7@users.noreply.github.com>
2022-09-11 21:05:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metotlar_Rekürsif_ve_Extension_Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
